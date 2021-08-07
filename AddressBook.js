72ab48486d2e4b58723d49a1caf2f89aed3f149b
lavanya-pg <lavanyapg8@gmail.com>
2021-08-07 15:52:55 +0530

    Added: Ability to Create a Contact in AddressBook

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,27 @@
 console.log("Welcome to the AddressBook Program");
+class Contact
+{
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNo;
+    email;
+
+    constructor(...params)
+    {
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phoneNo = params[6];
+        this.email = params[7];
+    }
+}
+
+let contact1 = new Contact("Lavanya", "Gangadharan", "Kann", "Chennai", "Tamilnadu",609842,9086543215, "lavanya@gmail.com")
+console.log(contact1)
